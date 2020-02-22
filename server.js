75e25cf7c97115a9644e09eb37adeab286bdd7d9
Ekampreet Singh <ekampreet.singh@block8.com>
2020-02-23 00:41:36 +0530

    added todoRoutes

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const todoRoutes = express.Router();
 const PORT = 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/todos", todoRoutes);
 
 mongoose.connect("mongodb://127.0.0.1:27017/todos", { useNewUrlParser: true });
 const connection = mongoose.connection;
